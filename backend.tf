a801ba6a1fb146f0337825e65e8835ed6644330d
david johnston <davidjohnston1100@gmail.com>
2024-07-15 15:01:32 -0500

    updated backend.tf file

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-pipeline-deployement-backend-state-bucket-us-east-2"
     key            = "terraform-state-key"
     region         = "us-east-2"
-    dynamodb_table = "sagemaker-statelock-table"
+    dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
 }
